1db0d02f545bb034ccff3130fe11098270e6554b
folathecoder <officialakinloyefolarin@gmail.com>
2021-09-18 11:58:02 +0100

    Feature Section ==== Implemented Background Image

--- src/components/FeatureSection/FeatureSectionStyles.js
+++ src/components/FeatureSection/FeatureSectionStyles.js
@@ -1,12 +1,36 @@
 import styled from "styled-components";
 import { QUERIES } from "../../helpers/MediaQueries";
+import featureBackground from "../../assets/images/bg-pattern.svg";
 
 export const FeatureContainer = styled.section`
   max-width: 100%;
   min-height: 0rem;
+  padding: 4rem 1rem 2rem 1rem;
+  background-image: url(${featureBackground});
+  background-repeat: no-repeat;
+  background-size: 36.0625rem 12.6875rem;
+  background-position: -20rem 36rem;
+
+  @media ${QUERIES.mobile} {
+    background-size: 36.0625rem 12.6875rem;
+    background-position: -15rem 33rem;
+  }
+
+  @media ${QUERIES.tabletMini} {
+    background-size: 42.5rem auto;
+    background-position: -15rem 31rem;
+  }
+
+  @media ${QUERIES.tablet} {
+    background-size: 62.5rem 22rem;
+    background-position: -30rem 32rem;
+  }
+
+  @media ${QUERIES.megaDesktop} {
+    background-image: none;
+  }
 `;
 export const FeatureWrap = styled.div`
-  /* background-color: red; */
   max-width: var(--max-container);
   min-height: 30rem;
   margin: var(--center-container);
@@ -26,14 +50,15 @@ export const FeatureTabs = styled.div`
   min-height: 4rem;
   margin: var(--center-container);
   margin-top: 2rem;
-  /* background-color: pink; */
   display: flex;
   flex-direction: column;
-  border-bottom: 0.1rem solid var(--color-sec-400);
-  
+  border-bottom: 0.1rem solid transparent;
+  border-top: 0.1rem solid var(--color-sec-400);
 
-  @media ${QUERIES.tablet} {
+  @media ${QUERIES.mobile} {
     flex-direction: row;
+    border-top: 0.1rem solid transparent;
+    border-bottom: 0.1rem solid var(--color-sec-400);
   }
 
   & > * {
@@ -48,44 +73,57 @@ export const FeatureTab = styled.button`
   cursor: pointer;
   font-size: 1rem;
   line-height: 1.5rem;
-  border-bottom: 0.2rem solid
-    ${({ clicked, index }) => (clicked === index ? "var(--color-pry-300)" : "transparent")};
+  border-bottom: ${({ clicked, index }) =>
+    clicked === index
+      ? "0.2rem solid var(--color-pry-300)"
+      : "0.1rem solid var(--color-sec-400)"};
 
-  @media ${QUERIES.tabletMini} {
+  @media ${QUERIES.tablet} {
+    border-bottom: ${({ clicked, index }) =>
+      clicked === index
+        ? "0.2rem solid var(--color-pry-300)"
+        : "0.1rem solid transparent"};
   }
 `;
 
 export const FeatureContentWrap = styled.div`
-    max-width: 100%;
-    min-height: 7rem;
-    margin: 4rem 0rem;
-    /* background-color: pink; */
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  max-width: 100%;
+  min-height: 7rem;
+  margin: 2rem 0rem;
+  /* background-color: pink; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  display: ${({ clicked, index }) => (clicked === index ? "flex" : "none")};
+
+  & > * {
+    flex: 1;
+    padding: 1rem;
+  }
+
+  @media ${QUERIES.tablet} {
+    flex-direction: row;
     text-align: left;
-    display: ${({clicked, index}) => clicked === index ? "flex" : "none"};
-
-    & > * {
-        flex: 1;
-        padding: 1rem;
-    }
-
-    @media ${QUERIES.tablet} {
-        flex-direction: row;
-    }
-`
+    margin: 4rem 0rem;
+  }
+`;
 export const FeatureImage = styled.img``;
 export const FeatureContent = styled.div`
   @media ${QUERIES.tablet} {
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
   }
 `;
 export const FeatureContentInner = styled.div`
-    max-width: 27.8125rem;
-    /* background-color: red; */
+  max-width: 27.8125rem;
+  margin-top: 2rem;
+  /* background-color: red; */
+
+  @media ${QUERIES.tablet} {
+    margin-top: 0rem;
+  }
 `;
 export const FeatureContentHeader = styled.h3`
   font-size: 1.5rem;
@@ -98,5 +136,5 @@ export const FeatureContentHeader = styled.h3`
   }
 `;
 export const FeatureContentSubHeader = styled.p`
-    margin: 1rem 0rem;
+  margin: 1rem 0rem;
 `;
